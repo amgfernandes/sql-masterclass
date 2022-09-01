3ccac2cfaab22005795abd70e347cc7c38ccd667
amgfernandes <amgfernandes@gmail.com>
2022-09-01 17:08:46 +0200

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -1,4 +1,9 @@
-SELECT MEMBER_ID,
-	FIRST_NAME
-FROM TRADING.MEMBERS
-WHERE REGION != 'Australia';
+SELECT
+  DATE_TRUNC('MON', txn_date)::DATE AS calendar_month,
+  SUM(CASE WHEN txn_type = 'BUY' THEN quantity ELSE 0 END) AS buy_quantity,
+  SUM(CASE WHEN txn_type = 'SELL' THEN quantity ELSE 0 END) AS sell_quantity
+FROM trading.transactions
+WHERE txn_date BETWEEN '2020-01-01' AND '2020-12-31'
+  AND ticker = 'ETH'
+GROUP BY calendar_month
+ORDER BY calendar_month;
